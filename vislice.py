517af6d0641d3b937b4e8fe159fc15caabf44c3d
Jan Malej <jan.malej12@gmail.com>
2022-05-18 14:25:42 +0200

    spletni 11.5.

--- vislice.py
+++ vislice.py
@@ -1,23 +1,44 @@
 import bottle
 import model
 
+
 vislice = model.Vislice()
+
+
 
 @bottle.get("/img/<file>")
 def staticne_slike(file):
-    return bottle.static_file(file, root = "img")
+    return bottle.static_file(file, root="img")
+
 
 @bottle.get("/")
-def prikazi_osnovno_stran():
+def osnovno():
     return bottle.template("index")
+
 
 @bottle.post("/igra/")
 def nova_igra():
-    novi_id = vislice.nova_igra()
-    return bottle.redirect(f"/igra/{novi_id}")
+    nov_id = vislice.nova_igra()
+    return bottle.redirect(f"/igra/{nov_id}")
+
 
 @bottle.get("/igra/<id_igre:int>")
 def pokazi_igro(id_igre):
-    return bottle.template("igra", igra=vislice.igre[id_igre][0])
+    return bottle.template("igra", id_igre=id_igre, igra=vislice.igre[id_igre][0])
+
+
+def preveri_vnos(crka):
+    return len(crka) == 1 and ("A" <= crka <= "Z" or crka in "ČŽŠ")
+
+
+@bottle.post('/igra/<id_igre:int>')
+def ugibaj(id_igre):
+    crka = bottle.request.forms.getunicode('crka').upper()
+    if preveri_vnos(crka):
+        vislice.ugibaj(id_igre, crka)
+        return pokazi_igro(id_igre)
+    else:
+        return f"<p>To ni dovoljena črka: {crka}</p>"
+
 
 bottle.run(reloader=True, debug=True)
